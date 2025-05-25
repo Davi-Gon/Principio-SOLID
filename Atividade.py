312da264d7f3574f82b306619e1214fb5a6018fb
davi-goncalves <davisilvagoncalves0@gmail.com>
2025-05-25 15:03:00 -0300

    Update Atividade.py

--- Atividade.py
+++ Atividade.py
@@ -1,17 +1,17 @@
 class SomaSub: 
-    def soma(self, a, b): #metodo/Função; o que ela faz
+    def soma(self, a, b):
         soma = a + b
         self.soma = soma
 
-    def sub(self, a, b): #metodo/Função; o que ela faz
+    def sub(self, a, b):
         sub = a - b
         self.sub = sub
 
-    def apresentar(self): #metodo/Função; o que ela faz
+    def apresentar(self):
         print(f"Soma: {self.soma}")
         print(f"sub : {self.sub}")
 
-cal = SomaSub() #é um objeto da classe SomaSub; e o self representa ele
-cal.soma(20, 30) #chamada de cada método para o objeto cal
+cal = SomaSub()
+cal.soma(20, 30)
 cal.sub(20, 30)
 cal.apresentar()
